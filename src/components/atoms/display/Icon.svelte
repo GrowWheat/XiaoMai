cbb91674918808685d3412c08b6f7baf4c15796f
XiaoMai <3205894053@qq.com>
2026-09-15 10:39:32 +0800

    Add files via upload

---
+++ src/components/atoms/display/Icon.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import OfflineIcon from "@xiaomai/iconify-offline";
+import { addCollection } from "@xiaomai/iconify-offline-functions";
+import type { ComponentProps } from "svelte";
+import { localIconCollections } from "@/generated/local-icon-collections";
+
+for (const collection of localIconCollections) addCollection(collection);
+
+let { icon, ...props }: ComponentProps<typeof OfflineIcon> = $props();
+</script>
+
+<OfflineIcon {icon} {...props} />
